7505e427638a1bdb7504b36629c78e8f3285ad0a
wutchara <SSTMUHAM@gmail.com>
2025-02-02 22:23:22 +0700

    add new topic

---
+++ missing-number/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {};
+
+const input1 = [3, 0, 1];
+console.log("input1", missingNumber(input1));
+
+const input2 = [0, 1];
+console.log("input2", missingNumber(input2));
+
+const input3 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+console.log("input3", missingNumber(input3));
